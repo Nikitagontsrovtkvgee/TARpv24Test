07a6b79c06e5e300733257e381b998c258090506
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:52:56 +0200

    Test

---
+++ Test/Test/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha if ja else konsoolirakendus, kus " +
+                "kontrollitakse stringi abil värvi vastavust");
+
+            Console.WriteLine("Värvida valikuks on: red, blue; green ja white");
+
+            Console.WriteLine("Peab käsitlema juhust, kus vastaja ei sisesta " +
+                "eelpool sisestatud värvi");
+            string color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("red");
+
+
+            }
+        }
+    }
+}
