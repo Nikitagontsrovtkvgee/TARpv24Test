e62b3bd1c772221826daa5ad71c525f3cd5ac1aa
Opilane TTHK <opilane@tthk.ee>
2025-02-05 14:54:03 +0200

    CoinCalculator

---
+++ CoinCalculator/CoinCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CoinCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            double coinDiameter = 25.75;
+            double earthRadius = 6378000000;
+            double aroundEarth = 2 * Math.PI * earthRadius;
+
+            Console.WriteLine("Maa ümbermõõt: " + aroundEarth);
+
+
+            double result = aroundEarth / coinDiameter;
+
+            Console.WriteLine("Ümber maa mahub nii" +
+                "palju kahe euroseid " + result);
+
+        }
+    }
+}
